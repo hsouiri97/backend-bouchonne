cfcc63e661ca5238de9bbf088b2a154e38bc8efa
hsouiri97 <hamza.souiri97@gmail.com>
2020-04-06 14:55:13 +0100

    2-fixing-path

--- api/auth.php
+++ api/auth.php
@@ -4,14 +4,17 @@
     header('Access-Control-Allow-Methods:  GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
     header('Content-Type: application/json');
-    
-    $client_info = file_get_contents("client_info.json");
-    $test2 = file_get_contents("test2.json");
+
+    $client_info = file_get_contents("bouchons/client_info.json");
+    $error = file_get_contents("bouchons/error.json");
 
     if($_SERVER['REQUEST_METHOD'] == 'GET') {
         $name = $_GET["name"];
         if($name === "client_info") {
             echo $client_info;
+        }
+        if ($name === "error") {
+            echo $error;
         }
     }
 
